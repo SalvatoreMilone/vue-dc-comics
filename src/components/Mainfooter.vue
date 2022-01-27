68cb3b9f081aa26bca2fa07e7c6238f9275e6358
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-01-27 17:02:13 +0100

    mainfooter

--- src/components/Mainfooter.vue
+++ src/components/Mainfooter.vue
@@ -1,7 +1,52 @@
 <template>
   <div class="">
     <div class="container">
+      <div class="subcontainer">
 
+        <div>
+          <span>DC COMICS</span>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+          </ul>
+          <span>SHOP</span>
+          <ul>
+            <li>Shop DC</li>
+            <li>Collectibles</li>
+          </ul>
+        </div>
+
+        <div>
+          <span>DC</span>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+          </ul>
+        </div>
+
+        <div>
+          <span>SITES</span>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+          </ul>
+        </div>
+
+
+      </div>
+        <img src="../assets/img/dc-logo-bg.png" >
     </div>
   </div>
 </template>
@@ -22,6 +67,45 @@ export default {
 <style scoped lang="scss">
 .container{
   height: 400px;
-  background-color: red;
+  background-image: url(../assets/img/footer-bg.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  color: white;
+  overflow: hidden;
 }
+.subcontainer{
+  display: flex;
+  width: 100%;
+}
+span{
+  font-size: 22px;
+}
+
+ul{
+  margin-top: 15px;
+  margin-bottom: 10px
+
+}
+
+.container > div ul{
+  list-style:none;
+}
+
+.subcontainer > div{
+  margin-right: 40px
+}
+
+.subcontainer > div li{
+  opacity: .8;
+  font-weight: 100px;
+}
+
+img{
+  position: relative;
+  right: 0px;
+}
+
 </style>
