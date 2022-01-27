8d0274f2eb244de315f6581c6f2277ef81107766
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-01-27 16:23:50 +0100

    topfooter

--- src/components/Topfooter.vue
+++ src/components/Topfooter.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="">
     <div class="container">
-
+      <div class="icons">
+        <div><img src="../assets/img/buy-comics-digital-comics.png">DIGITAL COMICS</div>
+        <div><img src="../assets/img/buy-comics-merchandise.png">DC MERCHANDISE</div>
+        <div><img src="../assets/img/buy-comics-subscriptions.png">SUBSCRIPTION</div>
+        <div><img src="../assets/img/buy-comics-shop-locator.png">COMIC SHOP LOCATOR</div>
+        <div><img src="../assets/img/buy-dc-power-visa.svg">DC POWER VISA</div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +27,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .container{
-  height: 200px;
+  height: var(--height1);
   background-color: var(--maincolor);
+  color: white;
+  font-size: 13px;
 }
+.icons{
+  height: var(--height1);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  &>div{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &>div img{
+    height: 60px;
+    margin-right: 10px
+  }
+}
+
 </style>
