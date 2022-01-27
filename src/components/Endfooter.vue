174f8f8098e801742709b2a58cd26037b2f8a235
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-01-27 17:14:09 +0100

    footerend

--- src/components/Endfooter.vue
+++ src/components/Endfooter.vue
@@ -2,6 +2,19 @@
   <div class="">
     <div class="container">
 
+      <div>
+        <button>SIGN-UP NOW!</button>
+      </div>
+
+      <div>
+        <span>FOLLOW US</span>
+        <img src="../assets/img/footer-facebook.png">
+        <img src="../assets/img/footer-periscope.png">
+        <img src="../assets/img/footer-pinterest.png">
+        <img src="../assets/img/footer-twitter.png">
+        <img src="../assets/img/footer-youtube.png">
+      </div>
+
     </div>
   </div>
 </template>
@@ -23,5 +36,31 @@ export default {
 .container{
   height: 100px;
   background-color: #303030;
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+}
+
+button{
+  padding: 10px 15px;
+  background-color: transparent;
+  border: 3px solid var(--maincolor);
+  color: white;
+  font-size: 20px;
+}
+
+.container div:nth-child(2){
+  color: var(--maincolor);
+  display: flex;
+  align-items: center;
+
+  img{
+    margin: 0 10px;
+  }
+
+  span{
+    margin-right: 15px;
+    font-size: 20px;
+  }
 }
 </style>
